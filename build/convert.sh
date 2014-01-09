08895dacf72ed527167d9319bd66417012065f43
KiD <kid.zcm@qq.com>
2014-01-09 13:04:01 +0800

    修正手滑==

--- build/convert.sh
+++ build/convert.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+#need perl
 
-if [ $? < 3 ]; then
+if [ -z $1 ] || [ -z $2 ]; then
+        echo "Bad argv."
+        echo "$0 <form_dir> <to_dir>"
 	exit
 fi
 
 find $1 -name "*.txt" 2>/dev/null | while read fname
 do
-	wname=${fname/$1/$2}
+	wname=${fname/$1/$2\/}
 	echo $fname $wname
 	piconv -f cp932 -t cp936 $fname > $wname
 done
