f2d78c5ffc87b7bffea5872aea6d177da1a48e41
KiD <kid.zcm@qq.com>
2014-02-18 12:56:39 +0800

    增加文本导入

---
+++ build/ImportScripts.cmd
@@ -0,0 +1,8 @@
+call Config.cmd
+@echo off
+
+path %PATH%;%DIR_PYTHON%
+python font_tool.py -r %DIR_TEMP%\txtchs\ %DIR_TEMP%\font\t1.TBL
+compress.bat %DIR_TEMP%\txtchs txt
+python wa2_eboot_tool.py -i %DIR_TEMP%\EBOOT.ELF %DIR_TEMP%\txtchs
+pause
