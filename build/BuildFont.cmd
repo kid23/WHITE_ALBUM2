1d2095d3d69cff2cc58f8cd7a7d1e757223dce8a
KiD <kid.zcm@qq.com>
2014-02-20 22:40:28 +0800

    脚本修正

--- build/BuildFont.cmd
+++ build/BuildFont.cmd
@@ -1,7 +1,8 @@
 call Config.cmd
 @echo off
 
-"%WORK_SPACE%\binary\FontMake.exe" -fwa2 "%DIR_TEMP%\font\t1.txt" "%DIR_TEMP%\font\t1.txt"
+path %PATH%;%DIR_PYTHON%
+"%WORK_SPACE%\binary\FontMake.exe" -fwa2 "%DIR_TEMP%\font\all.txt" "%DIR_TEMP%\font\all.txt"
 copy "%DIR_TEMP%\data.dar_unpacked\ddspkg\02309.pkgdds" "%DIR_TEMP%\font"
 move font1.png "%DIR_TEMP%\font\02309.pkgdds_000.png"
 move font3.png "%DIR_TEMP%\font\02309.pkgdds_002.png"
@@ -13,6 +14,6 @@ python import_file.py -ip "%DIR_TEMP%\font\02309.pkgdds"
 call compress.bat "%DIR_TEMP%\font\" pkgdds
 for /f "delims=" %%i in ("%DIR_TEMP%\font\02309.pkgdds") do (
 set lzname=%%~ni
-copy "%DIR_TEMP%\font\02309.pkgdds.elzma" "%DIR_OUTPUT%\dar\%lzname:~0,5%.elzma"
+copy "%DIR_TEMP%\font\02309.pkgdds.elzma" "%DIR_OUTPUT%\dar\%lzname%.elzma"
 )
 pause
--- build/ImportScripts.cmd
+++ build/ImportScripts.cmd
@@ -2,7 +2,8 @@ call Config.cmd
 @echo off
 
 path %PATH%;%DIR_PYTHON%
-python font_tool.py -r %DIR_TEMP%\txtchs\ %DIR_TEMP%\font\t1.TBL
+python font_tool.py -r %DIR_TEMP%\txtchs\ %DIR_TEMP%\font\all.TBL
 call compress.bat %DIR_TEMP%\txtchs txt
-python wa2_eboot_tool.py -i %DIR_TEMP%\EBOOT.ELF %DIR_TEMP%\txtchs
+rem python wa2_eboot_tool.py -i %DIR_TEMP%\EBOOT.ELF %DIR_TEMP%\txtchs
+copy /y "%DIR_TEMP%\txtchs\*.elzma" "%DIR_TEMP%\eboot\"
 pause
