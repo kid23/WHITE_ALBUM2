f7746a6e0879be9498f490430112ad78c2651b6f
KiD <kid.zcm@qq.com>
2014-02-22 01:02:34 +0800

    可恶的变量延迟，居然又遇上了。。。直接写文件名好了==

--- build/BuildFont.cmd
+++ build/BuildFont.cmd
@@ -1,8 +1,8 @@
 call Config.cmd
-@echo off
+echo off
 
 path %PATH%;%DIR_PYTHON%
-"%WORK_SPACE%\binary\FontMake.exe" -fwa2 "%DIR_TEMP%\font\all.txt" "%DIR_TEMP%\font\all.txt"
+"%WORK_SPACE%\binary\FontMake.exe" -fwa2 "%DIR_TEMP%\font\final.txt" "%DIR_TEMP%\font\final.txt"
 copy "%DIR_TEMP%\data.dar_unpacked\ddspkg\02309.pkgdds" "%DIR_TEMP%\font"
 move font1.png "%DIR_TEMP%\font\02309.pkgdds_000.png"
 move font3.png "%DIR_TEMP%\font\02309.pkgdds_002.png"
@@ -12,8 +12,5 @@ move font3.png "%DIR_TEMP%\font\02309.pkgdds_002.png"
 "%PS3_TOOL%\dds2gtf.exe" "%DIR_TEMP%\font\02309.pkgdds_002.dds"
 python import_file.py -ip "%DIR_TEMP%\font\02309.pkgdds"
 call compress.bat "%DIR_TEMP%\font\" pkgdds
-for /f "delims=" %%i in ("%DIR_TEMP%\font\02309.pkgdds") do (
-set lzname=%%~ni
-copy "%DIR_TEMP%\font\02309.pkgdds.elzma" "%DIR_OUTPUT%\dar\%lzname%.elzma"
-)
+copy "%DIR_TEMP%\font\02309.pkgdds.elzma" "%DIR_OUTPUT%\dar\02309.elzma"
 pause
