2424aa37965f524f8069e5cd8b088228caffc031
KiD <kid.zcm@qq.com>
2014-02-25 00:18:10 +0800

    增加注音文本分析，字库制作

--- build/ImportScripts.cmd
+++ build/ImportScripts.cmd
@@ -2,7 +2,7 @@ call Config.cmd
 @echo off
 
 path %PATH%;%DIR_PYTHON%
-python font_tool.py -r %DIR_TEMP%\txtchs\ %DIR_TEMP%\font\all.TBL
+python font_tool.py -r %DIR_TEMP%\txtchs\ %DIR_TEMP%\font\final2.TBL %DIR_TEMP%\font\zhuyin.TBL
 call compress.bat %DIR_TEMP%\txtchs txt
 rem python wa2_eboot_tool.py -i %DIR_TEMP%\EBOOT.ELF %DIR_TEMP%\txtchs
 copy /y "%DIR_TEMP%\txtchs\*.elzma" "%DIR_TEMP%\eboot\"
