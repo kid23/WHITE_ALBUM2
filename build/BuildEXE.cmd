887c91163fe7ea99a8018eb057d8bb8bc0228dad
KiD <kid.zcm@qq.com>
2014-04-21 20:13:24 +0800

    增加制作日文还原补丁

--- build/BuildEXE.cmd
+++ build/BuildEXE.cmd
@@ -5,4 +5,11 @@ path %PATH%;%DIR_PYTHON%
 del "%DIR_OUTPUT%\dar.zip"
 python setup.py py2exe -d "%DIR_OUTPUT%"
 rmdir /S /Q build\ 
+move /y "%DIR_OUTPUT%\import_file.exe" "%DIR_OUTPUT%\WA2_PS3_CHS_PATCH.exe"
+
+del "%DIR_OUTPUT%\darjpn.zip"
+python setup_jpn.py py2exe -d "%DIR_OUTPUT%"
+rmdir /S /Q build\ 
+move /y "%DIR_OUTPUT%\import_file.exe" "%DIR_OUTPUT%\WA2_PS3_JPN_RESTORE.exe"
+
 pause
---
+++ build/setup_jpn.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+import os
+import py2exe
+import zipfile
+
+def zip_dar(): 
+    f = zipfile.ZipFile('../output/darjpn.zip','w',zipfile.ZIP_DEFLATED) 
+    for dirpath, dirnames, filenames in os.walk('../output/darjpn/'): 
+        for filename in filenames: 
+            f.write(os.path.join(dirpath,filename), filename) 
+    f.close() 
+
+zip_dar()    
+data = open("../output/darjpn.zip", "rb").read()
+excludes = list()
+for line in open("pylist.txt", "r").readlines():
+    line=line.strip('\n')
+    if (line.endswith('.pyo') or line.endswith('.pyd')):
+        line = line[:-4]
+    excludes.append(line)
+
+setup(
+    options = {'py2exe': 
+                      {
+                      'bundle_files': 1, 
+                      'compressed': 1, 
+                      'optimize': 2, 
+                      'ascii': 1, 
+                      'dll_excludes': ['w9xpopen.exe'],
+                      "excludes": excludes
+                      }
+              },
+    zipfile = None,
+    #author = 'KiD',
+    #version = "1.0",
+    console = [{
+        'script': 'import_file.py',
+        'other_resources': [(1,1,data)],
+        'icon_resources': [(1,'w1.ico')],
+        },],
+)
+
